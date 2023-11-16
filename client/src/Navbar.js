48223717c850b422eb2a1821afb69c2038dee8df
j0vanni <49281020+j0vanni@users.noreply.github.com>
2023-11-16 15:12:03 -0500

    added logo, basic frontend to be worked on

---
+++ client/src/Navbar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "./assets/reel_logo.png";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav>
+      <div>
+        <Link to="/">
+          <img src={logo} alt="Reel" className="logo" />
+        </Link>
+      </div>
+      <ul className="nav-links">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/upload">Upload</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/signup">Signup</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
