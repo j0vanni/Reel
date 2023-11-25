010b56b90a5bc4d2b3fc2860291bfd1ff340c814
j0vanni <49281020+j0vanni@users.noreply.github.com>
2023-11-24 21:14:39 -0500

    made the nav bar look more like a film reel & tidied up the video displays

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import logo from "./assets/reel_logo.png";
 import "./Navbar.css";
 
+//this will just be the blocks rendered above and below the links for looks
+const BlocksRow = ({width, position}) => {
+  const numberOfBlocks = Math.floor(width / 20);
+
+  return (
+    <div className={`blocks-row ${position === 'top' ? 'blocks-row-top' : 'blocks-row-bottom'}`}>
+      {Array(numberOfBlocks)
+        .fill(null)
+        .map((_, i) => (
+          <div className="block" key={i}></div>
+        ))}
+    </div>
+  )
+}
+
+
 function Navbar() {
+  const [windowWidth, setWindowWidth] = React.useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <nav className="nav-holder">
       <div>
@@ -11,6 +35,7 @@ function Navbar() {
           <img src={logo} alt="Reel" className="logo" />
         </Link>
       </div>
+      <BlocksRow width={windowWidth} position='top'/>
       <ul className="nav-links">
         <li>
           <Link to="/">Home</Link>
@@ -25,6 +50,7 @@ function Navbar() {
           <Link to="/signup">Signup</Link>
         </li>
       </ul>
+      <BlocksRow width={windowWidth} position='bottom'/>
     </nav>
   );
 }
